d8e087f96563b07431993d7253cf841bf8d7f7fd
k1ssa1 <samikissai.edu@gmail.com>
2024-06-27 00:19:59 +0200

    fix: add cors proxy to URL shortening feature

--- src/js/shortenUrl.js
+++ src/js/shortenUrl.js
@@ -2,9 +2,10 @@ document.getElementById("form").addEventListener("submit", async e => {
     e.preventDefault();
     let mypromise = new Promise(function(resolve,reject){
         let longURL = document.getElementById("form__input").value;
+        const proxyUrl = 'https://corsproxy.io/?';
         let endpoint = "https://cleanuri.com/api/v1/shorten"
         let xhr = new XMLHttpRequest()
-        xhr.open("POST", endpoint, true)
+        xhr.open("POST",  proxyUrl + encodeURIComponent(endpoint), true)
         xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
         xhr.onload = function(){
             if(xhr.status >= 200 && xhr.status < 300){
